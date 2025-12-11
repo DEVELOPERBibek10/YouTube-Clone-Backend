b9dfa1becc3d172f3bb576fd922ad2fcb2a28867
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2025-12-11 16:00:30 +0545

    configue all setup

--- src/app.js
+++ src/app.js
@@ -16,4 +16,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-export { app };
+// router imports
+
+import userRouter from "./routes/users.routes.js";
+
+app.use("/api/v1/users", userRouter);
+
+export default app;
