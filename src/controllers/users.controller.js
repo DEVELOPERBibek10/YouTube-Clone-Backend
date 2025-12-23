68bb91ea6ceea9cfe19d6159063108038f42dd32
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2025-12-23 21:32:52 +0545

    add currentPasswordChange controller

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -241,10 +241,30 @@ const getCurrentUser = asyncHandler(async (req, res) => {
   );
 });
 
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  const user = await User.findById(req.user?.id);
+
+  if (!user) throw new ApiError(400, "User doesn't exist.");
+
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+  if (!isPasswordCorrect) throw new ApiError(400, "Invalid old password");
+
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully!"));
+});
+
 export {
   registerUser,
   loginUser,
   logoutUser,
   refreshAccessToken,
   getCurrentUser,
+  changeCurrentPassword,
 };
